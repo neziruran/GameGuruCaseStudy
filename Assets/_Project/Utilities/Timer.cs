baedbc1609ef128f89a084ab3aa65e59a8c804cd
neziruran@gmail.com <neziruran@gmail.com>
2023-06-10 01:47:45 +0300

    utilities imported

---
+++ Assets/_Project/Utilities/Timer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Project.Utilities
+{
+    public class Timer : MonoBehaviour
+    {
+        public static Timer Instance
+        {
+            get
+            {
+                var instance = FindObjectOfType<Timer>();
+                if (instance == null)
+                {
+                    instance = new GameObject("Timer").AddComponent<Timer>();
+                }
+
+                return instance;
+            }
+        }
+
+        private void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
+        private IEnumerator WaitForAction(float seconds, Action callback)
+        {
+            yield return new WaitForSeconds(seconds);
+            if(callback != null)
+                callback.Invoke();
+        }
+        
+        public Coroutine TimerWait(float time, Action callback)
+        {
+            return StartCoroutine(WaitForAction(time, callback));
+        }
+    }
+}
