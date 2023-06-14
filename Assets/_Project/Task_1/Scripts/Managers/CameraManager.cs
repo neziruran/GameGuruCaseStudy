fee7bb5a62eff2a767d123c72ae1df5f4038db55
neziruran@gmail.com <neziruran@gmail.com>
2023-06-15 01:16:13 +0300

    minor changes on task 1

---
+++ Assets/_Project/Task_1/Scripts/Managers/CameraManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace _Project.Task_1
+{
+    public class CameraManager : MonoBehaviour
+    {
+        [HideInInspector] public Camera mainCamera;
+        private const float RefRatio = (float)1080 / 1920;
+        private void Awake()
+        {
+            mainCamera = Camera.main;
+        }
+
+        //set camera size
+        public void SetOrthographicSize(float size)
+        {
+            var currentRatio = (float)Screen.width / Screen.height;
+            var multiplier = currentRatio / RefRatio;
+            mainCamera.orthographicSize = (size)/multiplier;
+        }
+    }
+}
